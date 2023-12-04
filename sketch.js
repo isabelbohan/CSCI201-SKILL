e2b2417638d58bab3f0047fba57172ee0b5ea871
isabelbohan <152305166+isabelbohan@users.noreply.github.com>
2023-12-04 16:16:31 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,8 +5,8 @@
  * Evan Peck's example at
  * https://editor.p5js.org/evanpeck/sketches/O7MjzPFxb
  * 
- * Edward Talmage
- * 2023-11-27
+ * Isabel Bohan
+ * 2023-12-5
  *
  * Revisions
  * 1. 2022-06-28: added sound file loading and playing
@@ -19,160 +19,37 @@
  * 4. 2023-11-27: Clicking randomizes one (randomly-chosen) ball, not all of them, using the mouseClicked() handler.
  */
 
-const BOX_WIDTH  = 200;  // textbox dimensions
-const BOX_HEIGHT = 100;
-
-var balls = [];
-var sound;
-var testBall;
-
-function preload() {
-
-  sound = loadSound("apollo11.mp3");  // preload the sound file
-}
-
 function setup() {
-
-//  createCanvas(windowWidth, windowHeight);
-  createCanvas(600,400)
-
-  
-  noStroke();
-  
-  //sound.play();    // play the audio file once
-  sound.loop();  // play the sound file repeatedly
-  
-  for (var ballNum = 0; ballNum < 10; ballNum++) {
-  	balls[ballNum] = new Ball();  
-  }
-
-  let y = height;
-  testBall = new Ball();
-  testBall.size = 50;
-  testBall.ballX = 220;  // if ballX == 225, the ball just slides over the right edge
-  testBall.ballY = 300;
-  testBall.red = 0;
-  testBall.blue = 0;
-  testBall.green = 0;
-  testBall.speedX = 0;
-  testBall.speedY = 1.2;
-}
-
-function createBox() {
-  // prepare a box first
-  strokeWeight(4);
-  rect(0, 0, BOX_WIDTH, BOX_HEIGHT);
-  
-  textSize(32);           // size of the text (pixels)
-  fill(0, 102, 153);      // fill() takes R,G,B values as the color
-  // draw the text in the box (x,y,width,height) with the color in fill()
-  textAlign(CENTER);
-  text('Hello World!', BOX_WIDTH/2, BOX_HEIGHT/2);   
- 
+  createCanvas(400, 400);
 }
 
 function draw() {
+  background(220);
 
-  background(255);
-  createBox();
-  
-  testBallMove();  // a special ball to test corner collision
-  
-  for (var ballNum = 0; ballNum < balls.length; ballNum++) {
-    balls[ballNum].display();
-    balls[ballNum].checkForHitWall();
-    balls[ballNum].checkForHitBox();
-    balls[ballNum].moveBall();
-    
-//    if (mouseIsPressed) {
-//      balls[ballNum].randomize()
-//    }
-  }
-}
+  // Stem
+  fill(34, 139, 34); // Green color
+  rect(190, 200, 20, 100);
 
-function mouseClicked() {  
-//  if (mouseIsPressed) {
-    ballNum = int(random(balls.length));
-    balls[ballNum].randomize();
-//  }
-}
+  // Flower head
+  fill(255, 165, 0); // Orange color
+  ellipse(200, 150, 50, 50);
 
-function testBallMove() {
-  
-  testBall.display();
-  testBall.checkForHitWall();
-  testBall.checkForHitBox();
-  testBall.moveBall();
-}
+  // Petals
+  fill(255, 255, 0); // Yellow color
 
-class Ball { // Constructor
-  
-  constructor() {
-    // initial position
-    this.ballX = random(100, width)
-    this.ballY = random(100, height)
-    
-    // Dictates velocity + direction
-    this.speedY = random(-5, 5);
-    this.speedX = random(-5, 5);
-    
-    this.size = random(100);
-    
-    // How transparent the ball is
-    this.alpha = 100
-    
-    // RGB values for color
-    this.red   = random(255);
-    this.green = random(255);
-    this.blue  = random(255)
-  }
-  
-  display() {
-    fill(this.red, this.green, this.blue, this.alpha);
-    ellipse(this.ballX, this.ballY, this.size);
-  }
-  
-  randomize() {
-    this.speedY = random(-5, 5);
-    this.speedX = random(-5, 5);
-    this.red = random(255);
-    this.green = random(255);
-    this.blue = random(255);
-    this.size = random(100);
-  }
-  
-  checkForHitWall() {
-  
-    let radius = this.size / 2;
-    if ((this.ballY+radius) > height || (this.ballY-radius) < 0) {
-  	  this.speedY = -this.speedY;  
-  	}
-    if ((this.ballX+radius) > width  || (this.ballX-radius) < 0) {
-      this.speedX = -this.speedX;  
-    }
-  }
-  
-  checkForHitBox() {
-    
-    let radius = this.size / 2;
+  // Top-left petal
+  ellipse(180, 140, 30, 30);
 
-//    if (((this.ballX-radius) < BOX_WIDTH && (this.ballY-radius) < BOX_HEIGHT) || d < radius) {
-    if (((this.ballX-radius) < BOX_WIDTH && (this.ballY-radius) < BOX_HEIGHT)) {
-      // bump into the textbox, need to reverse direction
-      this.reverseBall();
-    }
-  }
-  
-  reverseBall() {
-    
-      this.speedX = -this.speedX;
-      this.speedY = -this.speedY;    
-  }
-  
-  moveBall() {
+  // Top-right petal
+  ellipse(220, 140, 30, 30);
 
-    this.ballX += this.speedX;
-  	this.ballY += this.speedY;
-  }
-  
+  // Bottom-left petal
+  ellipse(180, 160, 30, 30);
+
+  // Bottom-right petal
+  ellipse(220, 160, 30, 30);
+
+  // Flower center
+  fill(255, 0, 0); // Red color
+  ellipse(200, 150, 10, 10);
 }
